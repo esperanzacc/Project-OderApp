7f4b9dbc0ac54da5e993da0d66dfa611038e76b7
esperanzacc <v.esperanzac113@gmail.com>
2022-02-15 13:53:26 -0800

    Delete some String

--- OderApp/OrderConfirmationViewController.swift
+++ OderApp/OrderConfirmationViewController.swift
@@ -19,7 +19,7 @@ class OrderConfirmationViewController: UIViewController {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("init(coder:) has not been ")
     }
     
     
